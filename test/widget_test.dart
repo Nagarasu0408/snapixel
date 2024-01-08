7711794aa552db09cd29c96d76c3561e62a3e8df
NAGARASU.P <88180695+Nagarasu0408@users.noreply.github.com>
2024-01-08 15:43:04 +0530

    commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:snapixel/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Qpixel());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
