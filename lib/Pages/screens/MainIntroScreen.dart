3d73f655777779da730b5f6916466c161f4de4c0
NAGARASU.P <88180695+Nagarasu0408@users.noreply.github.com>
2024-01-08 21:35:29 +0530

    commit

--- lib/Pages/screens/MainIntroScreen.dart
+++ lib/Pages/screens/MainIntroScreen.dart
@@ -1,12 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+import '../LoginPage.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  PageController pageController = PageController();
+  var _EndPage = false;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Hello Mr"),),
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                _EndPage = (index == 2);
+              });
+            },
+            children: [
+              Container(
+                color: Colors.pink,
+              ),
+              Container(
+                color: Colors.amber,
+              ),
+              Container(
+                color: Colors.deepPurple,
+              ),
+            ],
+          ),
+          Container(
+            alignment: const Alignment(0, 0.7),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _EndPage
+                    ? GestureDetector(
+                        child:const Icon(
+                          Icons.arrow_back_outlined,
+                        ),
+                        onTap: () => pageController.previousPage(
+                            duration:const Duration(microseconds: 100),
+                            curve: Curves.bounceIn),
+                )
+                    : GestureDetector(
+                        child: Text("skip"),
+                        onTap: () => pageController.jumpToPage(2),),
+                 SmoothPageIndicator(
+                   controller: pageController,
+                   count: 3,
+                 ),
+                _EndPage?GestureDetector(
+                  child:Text('Done'),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()),);
+                  },
+                ):GestureDetector(
+                  child: const Icon(Icons.arrow_forward),
+                  onTap: () => pageController.nextPage(
+                      duration:const  Duration(microseconds: 100),
+                      curve: Curves.bounceIn),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
