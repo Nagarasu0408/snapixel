dbd711ecea44d45e71fc00df4c6f2608b6ff6fe7
Vignesh M <vickybuvanesh@gmail.com>
2024-01-11 10:16:41 +0530

    Intro commit

--- lib/Pages/screens/ExtraScreens/IntroScreen1.dart
+++ lib/Pages/screens/ExtraScreens/IntroScreen1.dart
@@ -10,19 +10,51 @@ class Intro1 extends StatelessWidget {
     return const SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(height: 100,),
+          Align(),
+          SizedBox(
+            height: 100,
+          ),
           GalleryImage(),
-          SizedBox(height: 50,),
-          Padding(
-            padding: EdgeInsets.only(right: 280),
-            child: Text("Title",style: TextStyle(fontSize: 30,fontFamily: 'Assistant',fontWeight: FontWeight.w900),),
+          SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                "Create your own library",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'Assistant',
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
           ),
           SizedBox(
             height: 15,
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Text('The Asgardians or Gods of Asgard are a fictional humanoid race of gods appearing in American comic books published by Marvel Comics. Some are taken from mythology while others are original creations.',style: TextStyle(fontSize: 15,fontFamily: 'Assistant',fontWeight: FontWeight.w800)),
+            padding: EdgeInsets.only(left: 45.0, right: 20.0),
+            child: Text(
+              'QPixel allows every users to upload their high quality photographs and let them to create their own library.',
+              style: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'Assistant',
+                  fontWeight: FontWeight.w800),
+                  textAlign: TextAlign.justify,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 45.0, right: 20.0, top: 10.0),
+            child: Text(
+              'Every masterpiece of the creators will be considered to be displayed in the user library in front of the world.',
+              style: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'Assistant',
+                  fontWeight: FontWeight.w800),
+                  textAlign: TextAlign.justify,
+            ),
           )
         ],
       ),
--- lib/Pages/screens/ExtraScreens/IntroScreen3.dart
+++ lib/Pages/screens/ExtraScreens/IntroScreen3.dart
@@ -7,21 +7,52 @@ class Intro3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SingleChildScrollView(
-      child:  Column(
+      child: Column(
         children: [
-          SizedBox(height: 100,),
+          SizedBox(
+            height: 100,
+          ),
           Explore(),
-          SizedBox(height: 50,),
-          Padding(
-            padding: EdgeInsets.only(right: 280),
-            child: Text("Title",style: TextStyle(fontSize: 30,fontFamily: 'Assistant',fontWeight: FontWeight.w900),),
+          SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(
+                "Expand your community",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'Assistant',
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
           ),
           SizedBox(
             height: 15,
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Text('The Asgardians or Gods of Asgard are a fictional humanoid race of gods appearing in American comic books published by Marvel Comics. Some are taken from mythology while others are original creations.',style: TextStyle(fontSize: 15,fontFamily: 'Assistant',fontWeight: FontWeight.w800)),
+            padding: EdgeInsets.only(left: 45.0, right: 20.0),
+            child: Text(
+              'Grow your fanbase by showcasing your masterpieces in the contest and build your community stronger and healthier.',
+              style: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'Assistant',
+                  fontWeight: FontWeight.w800),
+              textAlign: TextAlign.justify,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 45.0, right: 20.0, top: 10.0),
+            child: Text(
+              'Follow your favorite photographers profile to like, share, comment and download their content to your device.',
+              style: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'Assistant',
+                  fontWeight: FontWeight.w800),
+                  textAlign: TextAlign.justify,
+            ),
           )
         ],
       ),
